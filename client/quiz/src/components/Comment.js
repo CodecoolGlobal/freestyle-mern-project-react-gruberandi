97998987e2419a96683865f3bb023e32c5953465
naaaani <nandi.zalka@gmail.com>
2023-08-16 18:08:56 +0200

    Make popup work

--- client/quiz/src/components/Comment.js
+++ client/quiz/src/components/Comment.js
@@ -1,15 +1,20 @@
+import '../App.css';
 
-const Comment = ({ question, showComments }) => {
+const Comment = ({ question, showComments, toggleComments }) => {
 	if (showComments) {
 		return (
 			<div className="modal">
-				<div onClick={showComments} className="overlay"></div>
+				<div onClick={toggleComments} className="overlay"></div>
 				<div className="modal-content">
-					<h2>Hello Modal</h2>
-					<p>
-						{question.question}
-					</p>
-					<button className="close-modal" onClick={showComments}>
+					<h2>Comments</h2>
+					{question.comments.length > 0 ? (
+						question.comments.map((comment) => (
+							<div key={comment._id}>{comment.commentText}</div>
+						))
+					) : (
+						<div>No comments yet.</div>
+					)}
+					<button className="close-modal" onClick={toggleComments}>
 						CLOSE
 					</button>
 				</div>
