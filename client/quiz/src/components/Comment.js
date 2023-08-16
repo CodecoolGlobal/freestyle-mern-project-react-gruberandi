dd88e6eed6af8828ab3939403aef24e2e2214bcb
naaaani <nandi.zalka@gmail.com>
2023-08-16 17:09:26 +0200

    Add css

--- client/quiz/src/components/Comment.js
+++ client/quiz/src/components/Comment.js
@@ -1,28 +1,40 @@
 
 const Comment = ({ question, showComments }) => {
-    if (showComments) {
-        if (question.comments.length<1){
+	if (showComments) {
+		return (
+			<div className="modal">
+				<div onClick={showComments} className="overlay"></div>
+				<div className="modal-content">
+					<h2>Hello Modal</h2>
+					<p>
+						{question.question}
+					</p>
+					<button className="close-modal" onClick={showComments}>
+						CLOSE
+					</button>
+				</div>
+			</div>
+		)
+		// 			return (
+		// 				<>
+		// 					<div>There are no comments added yet.</div>
+		// 				</>
+		// 			)
+		// 		} else {
+		// 			return (
 
-            return (
-                <>
-                <div>There are no comments added yet.</div>
-                </>
-            )
-        } else {
-            return (
+		// 				<div>
+		// 					{question.comments.map((comment) => {
+		// 						return <div>{comment.commentText}</div>
 
-                <div>
-                    {question.comments.map((comment) => {
-                        return  <div>{comment.commentText}</div>
-                       
-                    })}
+		// 					})}
 
-                </div>
-            )
-        }
+		// 				</div>
+		// 			)
 
 
-    }
+
+	}
 
 }
 export default Comment
--- client/quiz/src/components/Quiz.js
+++ client/quiz/src/components/Quiz.js
@@ -3,62 +3,62 @@ import Answers from './Answers';
 import Question from './Question';
 
 const Quiz = () => {
-    const [questions, setQuestions] = useState([]);
-    const [randomQuestion, setRandomQuestion] = useState(null);
-    const [randomIndex, setRandomIndex] = useState('');
-    const [previousQuestions, setPreviousQuestions] = useState([]);
+	const [questions, setQuestions] = useState([]);
+	const [randomQuestion, setRandomQuestion] = useState(null);
+	const [randomIndex, setRandomIndex] = useState('');
+	const [previousQuestions, setPreviousQuestions] = useState([]);
 
-    const handleNewQuestion = (id) => {
-        const newList = [...previousQuestions];
-        newList.push(id);
-        setPreviousQuestions(newList);
-    }
+	const handleNewQuestion = (id) => {
+		const newList = [...previousQuestions];
+		newList.push(id);
+		setPreviousQuestions(newList);
+	}
 
-    const fetchQuestions = () => {
-        fetch('/api/question/')
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                setQuestions(data);
-            })
-            .catch((error) => {
-                console.error('Error fetching questions:', error);
-            });
-    };
+	const fetchQuestions = () => {
+		fetch('/api/question/')
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				return response.json();
+			})
+			.then((data) => {
+				setQuestions(data);
+			})
+			.catch((error) => {
+				console.error('Error fetching questions:', error);
+			});
+	};
 
-    useEffect(() => {
-        fetchQuestions();
-    }, []);
+	useEffect(() => {
+		fetchQuestions();
+	}, []);
 
-    useEffect(() => {
-        if (questions.length > 0) {
-            const randomIndexes = Math.floor(Math.random() * questions.length);
-            setRandomIndex(randomIndexes);
-            setRandomQuestion({...questions[randomIndexes]});
-        }
-    }, [questions, previousQuestions]);
-
-
-    if (randomQuestion) {
-        return (
-            <div>
-                <Question randomQuestion={randomQuestion}/>
-
-                <Answers randomQuestion={randomQuestion} onNewQuestion={handleNewQuestion} />
-            </div>
+	useEffect(() => {
+		if (questions.length > 0) {
+			const randomIndexes = Math.floor(Math.random() * questions.length);
+			setRandomIndex(randomIndexes);
+			setRandomQuestion({ ...questions[randomIndexes] });
+		}
+	}, [questions, previousQuestions]);
 
 
-        );
-    }
+	if (randomQuestion) {
+		return (
+			<div>
+				<Question randomQuestion={randomQuestion} />
+
+				<Answers randomQuestion={randomQuestion} onNewQuestion={handleNewQuestion} />
+			</div>
 
 
-    return (
-        <div>loading</div>
-    )
+		);
+	}
+
+
+	return (
+		<div>loading</div>
+	)
 
 };
 
