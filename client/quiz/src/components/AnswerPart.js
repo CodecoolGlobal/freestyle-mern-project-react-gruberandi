8a19656566b66874764713935f66bc17f7c054db
naaaani <nandi.zalka@gmail.com>
2023-08-17 11:47:10 +0200

    Add conditional coloring

--- client/quiz/src/components/AnswerPart.js
+++ client/quiz/src/components/AnswerPart.js
@@ -1,7 +1,26 @@
 const AnswerPart = ({ answer, onAnswer, isAnswered }) => {
+  
+  function divStyle() {
+    const trueColor = {
+      backgroundColor: "green"
+    };
+
+    const falseColor = {
+      backgroundColor: "red"
+    };
+
+    if (!isAnswered.answered) {
+      return {backgroundColor: "white"};
+    } else {
+      if (answer.correct) {
+        return trueColor;
+      } else if (!isAnswered.correct && !answer.correct)
+      return falseColor;
+    }
+  }
 
   return (
-    <div className="answer-card"
+    <div className="answer-card" style={divStyle()}
       onClick={() => {
         !isAnswered.answered && onAnswer(answer.correct);
       }}
@@ -9,6 +28,8 @@ const AnswerPart = ({ answer, onAnswer, isAnswered }) => {
       {answer.answer}
     </div>
   )
+
+
 
 }
 
